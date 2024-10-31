ba62ee41d2c4a2f3cac0c0729c227bbf9329f617
Mazen Mahmoud <bra74039@gmail.com>
2024-10-31 11:35:34 +0300

    upload project files

---
+++ src/Components/StartScreen.js
@@ -0,0 +1,20 @@
+import { useQuiz } from "../contexts/QuizContext";
+
+function StartScreen() {
+  const { numQuestions, dispatch } = useQuiz();
+
+  return (
+    <div className="start">
+      <h2 style={{textAlign:"center"}}>Test Your Information In Front End Developer</h2>
+      <h3>{numQuestions} questions to test your knowledge</h3>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "start" })}
+      >
+        Let's start
+      </button>
+    </div>
+  );
+}
+
+export default StartScreen;
